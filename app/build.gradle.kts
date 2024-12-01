509e93659973de233aa68bb8858501b2bc7227ec
Sanapol <junrey@list.ru>
2024-12-01 17:18:40 +0300

    git init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,14 @@
 plugins {
+    application
+    checkstyle
+    id("checkstyle")
     id("java")
+    id("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("com.github.ben-manes.versions") version "0.51.0"
+}
+
+application {
+    mainClass = "hexlet.code.Main"
 }
 
 group = "hexlet.code"
