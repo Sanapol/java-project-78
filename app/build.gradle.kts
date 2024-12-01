1c5204f26e6e5746224963214e410965246663b9
Sanapol <junrey@list.ru>
2024-12-01 17:26:44 +0300

    git init

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("com.github.ben-manes.versions") version "0.51.0"
+    id("io.freefair.lombok") version "8.11"
 }
 
 application {
@@ -19,7 +20,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.11.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
