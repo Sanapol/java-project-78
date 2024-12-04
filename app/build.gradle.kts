e0295a2a0a2cac74053c288d31450710313dcda4
Sanapol <junrey@list.ru>
2024-12-04 12:51:21 +0300

    update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,15 +1,9 @@
 plugins {
-    application
     checkstyle
     id("checkstyle")
     id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
-    id("com.github.ben-manes.versions") version "0.51.0"
     id("io.freefair.lombok") version "8.11"
-}
-
-application {
-    mainClass = "hexlet.code.Main"
 }
 
 group = "hexlet.code"
