83571fc865083ddf522cab460e0a6cda4ec75edd
Sanapol <junrey@list.ru>
2024-12-05 11:48:38 +0300

    update Validator, AllSchemaTest

--- app/src/test/java/hexlet/code/AllSchemaTest.java
+++ app/src/test/java/hexlet/code/AllSchemaTest.java
@@ -2,50 +2,79 @@ package hexlet.code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
-class StringSchemaTest {
+class AllSchemaTest {
 
     @Test
     void StringTest() {
         Validator v = new Validator();
 
-        StringSchema shema = v.string();
+        StringSchema schema = v.string();
 
-        shema.required();
-        shema.minLength(2);
-        shema.contains("A");
-        shema.minLength(4);
-        shema.contains("x");
+        schema.required();
+        schema.minLength(2);
+        assertTrue(schema.isValid("Go"));
 
-        assertTrue(shema.isValid("Alex"));
-        assertFalse(shema.isValid(""));
-        assertFalse(shema.isValid(null));
-        assertFalse(shema.isValid("Max"));
+        schema.contains("x");
+        assertTrue(schema.isValid("Max"));
+
+        schema.minLength(4);
+        schema.contains("A");
+
+        assertTrue(schema.isValid("Alex"));
+        assertFalse(schema.isValid(""));
+        assertFalse(schema.isValid(null));
+        assertFalse(schema.isValid("Max"));
     }
 
     @Test
     void NumbreTest() {
         Validator v = new Validator();
 
-        NumberSchema shema = v.number();
+        NumberSchema schema = v.number();
 
-        shema.required();
-        assertTrue(shema.isValid(-200));
+        schema.required();
+        assertTrue(schema.isValid(-200));
 
-        shema.positive();
-        assertTrue(shema.isValid(200));
+        schema.positive();
+        assertTrue(schema.isValid(200));
 
-        shema.range(2, 100);
-        assertTrue(shema.isValid(40));
+        schema.range(2, 100);
+        assertTrue(schema.isValid(40));
 
-        assertFalse(shema.isValid(101));
-        assertFalse(shema.isValid(-20));
-        assertFalse(shema.isValid(1));
-        assertFalse(shema.isValid(null));
+        assertFalse(schema.isValid(101));
+        assertFalse(schema.isValid(-20));
+        assertFalse(schema.isValid(1));
+        assertFalse(schema.isValid(null));
 
+    }
+
+    @Test
+    void MapTest() {
+        Validator v = new Validator();
+
+        MapSchema schema = v.map();
+
+        assertTrue(schema.isValid(null));
+
+        schema.required();
+        assertFalse(schema.isValid(null));
+
+        schema.sizeof(2);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("key1", "value1");
+
+        assertFalse(schema.isValid(map));
+
+        map.put("key2", "value2");
+
+        assertTrue(schema.isValid(map));
     }
 }
