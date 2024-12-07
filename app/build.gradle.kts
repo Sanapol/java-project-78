26c77226e34b8d14682b91b47e206a0c5822093d
Sanapol <junrey@list.ru>
2024-12-07 20:15:37 +0300

    update README.md, build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     checkstyle
+    jacoco
     id("checkstyle")
     id("java")
     id("se.patrikerdes.use-latest-versions") version "0.2.18"
-    id("io.freefair.lombok") version "8.11"
+    id("com.github.ben-manes.versions") version "0.51.0"
 }
 
 group = "hexlet.code"
@@ -16,8 +17,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.11.3"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.2")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.jacocoTestReport {
+    reports { xml.required.set(true) }
 }
