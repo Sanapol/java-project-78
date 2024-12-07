073c90ae69799568d6d1cf0bd7794d5df9ef715d
Sanapol <junrey@list.ru>
2024-12-07 18:52:48 +0300

    update Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -6,7 +6,7 @@ import hexlet.code.schemas.StringSchema;
 
 public class Validator {
 
-    public StringSchema string() throws RuntimeException {
+    public final StringSchema string() throws RuntimeException {
         try {
             return new StringSchema();
         } catch (Exception e) {
@@ -14,7 +14,7 @@ public class Validator {
         }
     }
 
-    public NumberSchema number() throws RuntimeException {
+    public final NumberSchema number() throws RuntimeException {
         try {
             return new NumberSchema();
         } catch (Exception e) {
@@ -23,7 +23,7 @@ public class Validator {
 
     }
 
-    public MapSchema map() throws RuntimeException {
+    public final MapSchema map() throws RuntimeException {
         try {
             return new MapSchema();
         } catch (Exception e) {
